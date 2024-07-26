8a3c1cdb4f104d8c59f6e8a0f4aba007ec186942
Homayoon <homayoon@localhost.localdomain>
2024-07-26 16:02:10 +0330

    اولیه

--- pickFile.py
+++ pickFile.py
@@ -6,11 +6,19 @@ if os.path.exists("Watched.txt") is False:
     open("Watched.txt", "x")
 
 movies_list = os.\
-  popen("ls /home/homayoon/other_things/11t_homa/Other_things").read().split("\n")
+  popen("ls").read().split("\n")
 #print(random.choice(movies_list))
+Picked = False
+while(Picked == False):
+    movie_name = random.choice(movies_list)
+    #print(movie_name)
+    with open("IdontWant.txt", "r") as idw, open("Watched.txt", "r") as watched:
+        idontwant_content = set(line.strip() for line in idw)
+        watched_content = set(line.strip() for line in watched)
+    if movie_name in idontwant_content or watched_content:
+        Picked = False
+    else:
+        print(movie_name)
+        Picked = True
 
-while(Picked):
-    movie_name = random.choice.(movies_list)
-    print(movie_name)
-    if 
 
