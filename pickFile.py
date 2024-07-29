4e969a56fc82775acddc79a9aff6ea1db8ca0caf
Homayoon <homayoon@localhost.localdomain>
2024-07-29 18:06:52 +0330

    update

--- pickFile.py
+++ pickFile.py
@@ -1,15 +1,16 @@
-import os, random
+import os, random, re
 
 try:
     open("IdontWant.txt", "x")
     open("Watched.txt", "x")
-    open("allMovies.txt", "x")
 except:
     pass
 
-movies_list = os.\
-  popen("ls -I \"*.txt\"").read().split("\n")
-#print(random.choice(movies_list))
+movies_list = os.popen("ls -I \"*.txt\"").read().split("\n")
+#for i in range(len(movies_list)):
+#    movies_list[i] = re.sub(r"[^a-zA-Z0-9 ]", " ", movies_list[i].strip()).lower()
+
+
 Picked = False
 while(Picked == False):
     with open("IdontWant.txt", "r+") as idw, open("Watched.txt", "r+") as watched:
@@ -22,7 +23,8 @@ while(Picked == False):
            # print("first")
             Picked = False
         else:
-            print(f"Do you want to watch {movie_name}? [y/n]")
+            movie_name_re = re.sub(r"[^a-zA-Z0-9 ]", " ", movie_name.strip()).lower()
+            print(f"Do you want to watch \"{movie_name_re}\"? [y/n]")
             ans = input()
             if ans == "y":
                 watched.write(movie_name + '\n')
